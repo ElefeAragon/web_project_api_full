290392e3086500a68d11e6130f547c89af9027a4
ElefeAragon <faragonaranjo69@gmail.com>
2026-08-15 08:45:07 -0700

    Corregir errores de lint, agregar celebrate/winston/express-winston al package.json, ajustar reglas de ESLint

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,13 +1,14 @@
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const User = require("../models/user");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 const {
   NotFoundError,
   BadRequestError,
   UnauthorizedError,
   ConflictError,
-} = require("../utils/errors");
-const { JWT_SECRET = "dev-secret-key" } = process.env;
+} = require('../utils/errors');
+
+const { JWT_SECRET = 'dev-secret-key' } = process.env;
 const getUsers = (req, res, next) => {
   User.find({})
     .then((users) => res.send(users))
@@ -15,39 +16,39 @@ const getUsers = (req, res, next) => {
 };
 const getUserById = (req, res, next) => {
   User.findById(req.params.userId)
-    .orFail(() => new NotFoundError("Usuario no encontrado"))
+    .orFail(() => new NotFoundError('Usuario no encontrado'))
     .then((user) => res.send(user))
     .catch((err) => {
-      if (err.name === "CastError") {
-        return next(new BadRequestError("ID de usuario inválido"));
+      if (err.name === 'CastError') {
+        return next(new BadRequestError('ID de usuario inválido'));
       }
       return next(err);
     });
 };
 const createUser = (req, res, next) => {
-  const { name, about, avatar, email, password } = req.body;
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
   bcrypt
     .hash(password, 10)
-    .then((hash) =>
-      User.create({
-        name,
-        about,
-        avatar,
-        email,
-        password: hash,
-      }),
-    )
+    .then((hash) => User.create({
+      name,
+      about,
+      avatar,
+      email,
+      password: hash,
+    }))
     .then((user) => {
       const userObject = user.toObject();
       delete userObject.password;
       res.status(201).send(userObject);
     })
     .catch((err) => {
-      if (err.name === "ValidationError") {
-        return next(new BadRequestError("Datos inválidos"));
+      if (err.name === 'ValidationError') {
+        return next(new BadRequestError('Datos inválidos'));
       }
       if (err.code === 11000) {
-        return next(new ConflictError("Este correo electrónico ya está registrado"));
+        return next(new ConflictError('Este correo electrónico ya está registrado'));
       }
       return next(err);
     });
@@ -62,11 +63,11 @@ const updateProfile = (req, res, next) => {
       runValidators: true,
     },
   )
-    .orFail(() => new NotFoundError("Usuario no encontrado"))
+    .orFail(() => new NotFoundError('Usuario no encontrado'))
     .then((user) => res.send(user))
     .catch((err) => {
-      if (err.name === "ValidationError" || err.name === "CastError") {
-        return next(new BadRequestError("Datos inválidos"));
+      if (err.name === 'ValidationError' || err.name === 'CastError') {
+        return next(new BadRequestError('Datos inválidos'));
       }
       return next(err);
     });
@@ -81,11 +82,11 @@ const updateAvatar = (req, res, next) => {
       runValidators: true,
     },
   )
-    .orFail(() => new NotFoundError("Usuario no encontrado"))
+    .orFail(() => new NotFoundError('Usuario no encontrado'))
     .then((user) => res.send(user))
     .catch((err) => {
-      if (err.name === "ValidationError" || err.name === "CastError") {
-        return next(new BadRequestError("Datos inválidos"));
+      if (err.name === 'ValidationError' || err.name === 'CastError') {
+        return next(new BadRequestError('Datos inválidos'));
       }
       return next(err);
     });
@@ -93,17 +94,17 @@ const updateAvatar = (req, res, next) => {
 const login = (req, res, next) => {
   const { email, password } = req.body;
   User.findOne({ email })
-    .select("+password")
+    .select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new UnauthorizedError("Correo o contraseña incorrectos"));
+        return Promise.reject(new UnauthorizedError('Correo o contraseña incorrectos'));
       }
       return bcrypt.compare(password, user.password).then((matched) => {
         if (!matched) {
-          return Promise.reject(new UnauthorizedError("Correo o contraseña incorrectos"));
+          return Promise.reject(new UnauthorizedError('Correo o contraseña incorrectos'));
         }
         const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
-          expiresIn: "7d",
+          expiresIn: '7d',
         });
         return res.send({ token });
       });
@@ -112,11 +113,11 @@ const login = (req, res, next) => {
 };
 const getCurrentUser = (req, res, next) => {
   User.findById(req.user._id)
-    .orFail(() => new NotFoundError("Usuario no encontrado"))
+    .orFail(() => new NotFoundError('Usuario no encontrado'))
     .then((user) => res.send(user))
     .catch((err) => {
-      if (err.name === "CastError") {
-        return next(new BadRequestError("ID de usuario inválido"));
+      if (err.name === 'CastError') {
+        return next(new BadRequestError('ID de usuario inválido'));
       }
       return next(err);
     });
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,31 +1,30 @@
-const validator = require("validator");
-const mongoose = require("mongoose");
+const validator = require('validator');
+const mongoose = require('mongoose');
 
-const urlRegex =
-  /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)$/;
+const urlRegex = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)$/;
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     minlength: 2,
     maxlength: 30,
-    default: "Jacques Cousteau",
+    default: 'Jacques Cousteau',
   },
   about: {
     type: String,
     minlength: 2,
     maxlength: 30,
-    default: "Explorador",
+    default: 'Explorador',
   },
   avatar: {
     type: String,
     default:
-      "https://practicum-content.s3.us-west-1.amazonaws.com/resources/moved_avatar_1604080799.jpg",
+      'https://practicum-content.s3.us-west-1.amazonaws.com/resources/moved_avatar_1604080799.jpg',
     validate: {
       validator(v) {
         return urlRegex.test(v);
       },
-      message: "URL no válida",
+      message: 'URL no válida',
     },
   },
   email: {
@@ -34,7 +33,7 @@ const userSchema = new mongoose.Schema({
     unique: true,
     validate: {
       validator: (v) => validator.isEmail(v),
-      message: "El correo electrónico no es válido",
+      message: 'El correo electrónico no es válido',
     },
   },
   password: {
@@ -44,4 +43,4 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-module.exports = mongoose.model("user", userSchema);
+module.exports = mongoose.model('user', userSchema);
